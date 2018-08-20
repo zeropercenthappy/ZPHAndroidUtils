e3054c8e4e36ba2228af50faa9833a95926d1ee0
zeropercenthappy <zeropercenthappy@foxmail.com>
2018-08-20 11:47:18 +0800

    add ResourceUtil

---
+++ utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/ResourceUtil.kt
@@ -0,0 +1,32 @@
+package com.zeropercenthappy.utilslibrary
+
+import android.content.Context
+
+object ResourceUtil {
+    /**
+     * @param context
+     * @param className id,layout,drawable,etc.
+     * @param resourceName resource name
+     */
+    fun getIdByName(context: Context, className: String, resourceName: String): Int {
+        val packageName = context.packageName
+        var id = 0
+        try {
+            val mClass = Class.forName("$packageName.R")
+            val classes = mClass.classes
+            var desireClass: Class<*>? = null
+            for (cls in classes) {
+                if (cls.name.split("\$")[1] == className) {
+                    desireClass = cls
+                    break
+                }
+            }
+            desireClass?.apply {
+                id = getField(resourceName).getInt(this)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return id
+    }
+}
