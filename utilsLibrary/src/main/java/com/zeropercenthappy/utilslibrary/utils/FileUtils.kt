254a77815fe517f82f38307003240ae84f9eee05
zeropercenthappy <zeropercenthappy@foxmail.com>
2020-01-13 16:19:18 +0800

    Optimize FileUtils

--- utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/FileUtils.kt
+++ utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/FileUtils.kt
@@ -1,23 +1,17 @@
 package com.zeropercenthappy.utilslibrary.utils
 
-import android.text.TextUtils
 import android.webkit.MimeTypeMap
 import java.io.*
+import java.util.*
 
 object FileUtils {
-    /**
-     * io流缓存大小
-     */
+
+
     private const val BUFFER_SIZE = 8192
 
-    /**
-     * 关闭io流
-     *
-     * @param closeables
-     */
     @JvmStatic
     fun closeIO(vararg closeables: Closeable?) {
-        if (closeables.isEmpty()) {
+        if (closeables.isNullOrEmpty()) {
             return
         }
         for (io in closeables) {
@@ -30,64 +24,75 @@ object FileUtils {
     }
 
     /**
-     * 检查文件是否存在，若不存在则新建文件
+     * Check the file is exist or not. If the file isn't exist, it will try to create all needs
+     * directory and itself.
      *
-     * @param file
-     * @return
+     * @param file  destination file
+     *
+     * @return      true or false(if not exist and create failure)
      */
     @JvmStatic
-    fun checkFileAndCreate(file: File?): Boolean {
-        if (file == null) {
-            return false
-        }
-        return if (!file.exists()) {
-            try {
+    fun checkFileAndCreate(file: File): Boolean {
+        return try {
+            val dir = file.parentFile
+            dir.mkdirs()
+            if (file.exists()) {
+                true
+            } else {
                 file.createNewFile()
-            } catch (e: Exception) {
-                e.printStackTrace()
-                false
+                file.exists()
             }
-        } else {
-            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
         }
     }
 
     /**
-     * 检查文件是否存在，若不存在则新建文件
+     * Check the file is exist or not. If the file isn't exist, it will try to create all needs
+     * directory and itself.
      *
-     * @param path
-     * @return
+     * @param path  destination file's absolute path
+     *
+     * @return      true or false(if not exist and create failure)
      */
     @JvmStatic
     fun checkFileAndCreate(path: String): Boolean {
-        if (TextUtils.isEmpty(path)) {
-            return false
-        }
-        val file = File(path)
-        return if (!file.exists()) {
-            try {
+        return try {
+            val file = File(path)
+            val dir = file.parentFile
+            dir.mkdirs()
+            if (file.exists()) {
+                true
+            } else {
                 file.createNewFile()
-            } catch (e: IOException) {
-                e.printStackTrace()
-                false
+                file.exists()
             }
-        } else {
-            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
         }
     }
 
     /**
-     * 通过InputStream写入文件
+     * Write file through InputStream.
+     *
+     * @param storageFile   storageFile
+     *
+     * @param inputStream   inputStream
+     *
+     * @return              write result
      */
     @JvmStatic
-    fun writeFileByIS(storageFile: File, inputStream: InputStream, append: Boolean): Boolean {
-        if (!checkFileAndCreate(storageFile)) {
-            return false
-        }
-        val bufferedOutputStream = BufferedOutputStream(FileOutputStream(storageFile))
-        val buffer = ByteArray(BUFFER_SIZE)
-        var currentLength: Int
+    fun writeFileByIS(storageFile: File, inputStream: InputStream): Boolean {
+        var bufferedOutputStream: BufferedOutputStream? = null
         try {
+            if (!checkFileAndCreate(storageFile)) {
+                return false
+            }
+            bufferedOutputStream = BufferedOutputStream(FileOutputStream(storageFile))
+            val buffer = ByteArray(BUFFER_SIZE)
+            var currentLength: Int
             while (true) {
                 currentLength = inputStream.read(buffer, 0, BUFFER_SIZE)
                 if (currentLength != -1) {
@@ -108,84 +113,74 @@ object FileUtils {
     }
 
     /**
-     * 向文件写入content
+     * Write string to a file.
      *
-     * @param filename 目标文件
-     * @param content  内容
-     * @param append   是否追加
-     * @return
+     * @param file      file
+     *
+     * @param content   content
+     *
+     * @param append    is append
+     *
+     * @return          write result
      */
     @JvmStatic
-    fun writeContent2File(filename: String, content: String, append: Boolean): Boolean {
-        try {
-            val bufferedWriter = BufferedWriter(FileWriter(filename, append))
-            bufferedWriter.write(content)
-            bufferedWriter.flush()
-            closeIO(bufferedWriter)
-            return true
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return false
-        }
+    fun writeString2File(file: File, content: String, append: Boolean) = try {
+        val bufferedWriter = BufferedWriter(FileWriter(file.absolutePath, append))
+        bufferedWriter.write(content)
+        bufferedWriter.flush()
+        closeIO(bufferedWriter)
+        true
+    } catch (e: IOException) {
+        e.printStackTrace()
+        false
     }
 
+
     /**
-     * 获取文件的mimeType
+     *  Get file's MimeType.
      *
-     * @param file
-     * @return
+     * @param file  Destination file
+     *
+     * @return      file's MimeType or null
      */
     @JvmStatic
     fun getFileMimeType(file: File): String? {
-        val path = file.absolutePath
-        if (TextUtils.isEmpty(path)) {
-            return null
-        }
-        val dotPosition = path.lastIndexOf(".")
-        if (dotPosition == -1) {
-            return null
-        }
-        var extension = path.substring(dotPosition + 1)
-        extension = extension.toLowerCase()
-        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        return getFileMimeTypeByExtension(file.extension)
     }
 
     /**
-     * 获取文件的mimeType
+     * Get file's MimeType.
      *
-     * @param path
-     * @return
+     * @param path  File's absolute path
+     *
+     * @return      file's MimeType or null
      */
     @JvmStatic
     fun getFileMimeType(path: String): String? {
-        if (TextUtils.isEmpty(path)) {
-            return null
-        }
-        val dotPosition = path.lastIndexOf(".")
-        if (dotPosition == -1) {
-            return null
-        }
-        var extension = path.substring(dotPosition + 1)
-        extension = extension.toLowerCase()
-        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        val file = File(path)
+        return getFileMimeTypeByExtension(file.extension)
     }
 
     /**
-     * 获取制定后缀名的mimeType
+     * Get file's MimeType by extension.
      *
      * @param extension 后缀名
-     * @return
+     *
+     * @return          file's MimeType or null
      */
     @JvmStatic
     fun getFileMimeTypeByExtension(extension: String): String? {
-        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase())
+        return MimeTypeMap
+            .getSingleton()
+            .getMimeTypeFromExtension(extension.toLowerCase(Locale.getDefault()))
     }
 
     /**
-     * 获取文件或目录的大小，单位：Byte
+     * Get file's size, unit：Byte
      *
-     * @param file
-     * @return
+     * @param file  file
+     *
+     * @return      file's size
      */
     fun getSize(file: File?): Long {
         var size: Long = 0
@@ -204,22 +199,24 @@ object FileUtils {
     }
 
     /**
-     * 复制文件
+     * Copy file.
      *
-     * @param sourceFile  源文件
-     * @param targetFile 目标文件
-     * @return
+     * @param sourceFile  source file
+     *
+     * @param destinationFile  destination file
+     *
+     * @return copy result
      */
-    fun copyFile(sourceFile: File, targetFile: File): Boolean {
+    fun copyFile(sourceFile: File, destinationFile: File): Boolean {
         if (!sourceFile.exists()) {
             // 若源文件不存在
             return false
         }
-        if (!deleteFile(targetFile)) {
+        if (!deleteFile(destinationFile)) {
             // 若旧目标文件删除失败
             return false
         }
-        if (!checkFileAndCreate(targetFile)) {
+        if (!checkFileAndCreate(destinationFile)) {
             // 若目标文件创建失败
             return false
         }
@@ -227,7 +224,7 @@ object FileUtils {
         var bufferedOutputStream: BufferedOutputStream? = null
         try {
             inputStream = FileInputStream(sourceFile)
-            bufferedOutputStream = BufferedOutputStream(FileOutputStream(targetFile, false))
+            bufferedOutputStream = BufferedOutputStream(FileOutputStream(destinationFile, false))
             val data = ByteArray(BUFFER_SIZE)
             while (true) {
                 val length = inputStream.read(data, 0, BUFFER_SIZE)
@@ -252,10 +249,11 @@ object FileUtils {
     }
 
     /**
-     * 删除文件或目录
+     * Delete file or directory
      *
-     * @param file
-     * @return
+     * @param file  destination file or directory
+     *
+     * @return      delete result
      */
     @JvmStatic
     fun deleteFile(file: File): Boolean {
