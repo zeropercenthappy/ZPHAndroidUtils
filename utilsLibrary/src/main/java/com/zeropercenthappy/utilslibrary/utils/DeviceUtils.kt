fe9163429687c5e2750bccda1a1056b28f788cfd
zeropercenthappy <zeropercenthappy@foxmail.com>
2020-03-31 18:16:40 +0800

    Update DeviceUtils;

--- utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/DeviceUtils.kt
+++ utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/DeviceUtils.kt
@@ -2,9 +2,7 @@ package com.zeropercenthappy.utilslibrary.utils
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Build
 import android.provider.Settings
-import android.text.TextUtils
 import java.io.File
 import java.util.*
 import java.util.regex.Pattern
@@ -24,16 +22,11 @@ object DeviceUtils {
     /**
      * 获取Android设备物理唯一标识符
      *
-     * 当Serial值获取不到（为unknown）时，将使用AndroidId来生成UUID，此时若是系统重置或是刷机，
-     * 再调用此方法获取到的唯一标识符将会变化
+     * 使用AndroidId来生成UUID，若是系统重置或是刷机后，再调用此方法获取到的唯一标识符将会变化
      */
     @SuppressLint("HardwareIds")
     @JvmStatic
     fun getUniqueID(context: Context): String {
-        val serial = Build::class.java.getField("SERIAL")[null].toString()
-        if (!TextUtils.equals("unknown", serial.toLowerCase(Locale.ROOT))) {
-            return UUID.nameUUIDFromBytes(serial.toByteArray()).toString()
-        }
         val androidId =
             Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
         return UUID.nameUUIDFromBytes(androidId.toByteArray()).toString()
