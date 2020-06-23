d0ae4f58af516072e65f88f77d67426fda829b99
zeropercenthappy <zeropercenthappy@foxmail.com>
2020-06-23 17:43:11 +0800

    Add lambda style log invoker;

--- utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/ZPHLogger.kt
+++ utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/utils/ZPHLogger.kt
@@ -8,10 +8,18 @@ interface ZPHLogger {
         var SHOW_LOG = true
     }
 
+    fun warn(execute: () -> String) {
+        warn(execute.invoke())
+    }
+
     fun warn(content: String) {
         if (SHOW_LOG) {
             Log.w(this::class.java.simpleName, content)
         }
+    }
+
+    fun debug(execute: () -> String) {
+        debug(execute.invoke())
     }
 
     fun debug(content: String) {
@@ -20,16 +28,28 @@ interface ZPHLogger {
         }
     }
 
+    fun info(execute: () -> String) {
+        info(execute.invoke())
+    }
+
     fun info(content: String) {
         if (SHOW_LOG) {
             Log.i(this::class.java.simpleName, content)
         }
     }
 
+    fun error(execute: () -> String) {
+        error(execute.invoke())
+    }
+
     fun error(content: String) {
         if (SHOW_LOG) {
             Log.e(this::class.java.simpleName, content)
         }
+    }
+
+    fun verbose(execute: () -> String) {
+        verbose(execute.invoke())
     }
 
     fun verbose(content: String) {
