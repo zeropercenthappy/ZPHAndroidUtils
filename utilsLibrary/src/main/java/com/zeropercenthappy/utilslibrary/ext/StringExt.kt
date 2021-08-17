81e23b347e9a81b708aa44d80ef4451769398735
zeropercenthappy <zeropercenthappy@foxmail.com>
2021-08-17 23:38:54 +0800

    Update StringExt；

--- utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/ext/StringExt.kt
+++ utilsLibrary/src/main/java/com/zeropercenthappy/utilslibrary/ext/StringExt.kt
@@ -1,4 +1,11 @@
+@file:Suppress("FunctionName", "unused")
+
 package com.zeropercenthappy.utilslibrary.ext
+
+import android.util.Base64
+import java.nio.charset.Charset
+
+const val STRING_EXT_SEPARATOR = ","
 
 fun String?.toByteSafely(defaultValue: Byte = 0): Byte {
     return try {
@@ -46,4 +53,62 @@ fun String?.toDoubleSafely(defaultValue: Double = 0.0): Double {
     } catch (e: Exception) {
         defaultValue
     }
+}
+
+fun String.toAscii(): String {
+    return map { it.code.toString(16) }.joinToString(separator = STRING_EXT_SEPARATOR)
+}
+
+fun String.asciiToString(): String {
+    return toInt(16).toChar().toString()
+}
+
+fun String.toBase64(): String {
+    return Base64.encodeToString(this.toByteArray(), Base64.DEFAULT)
+}
+
+fun String.base64ToString(): String {
+    return String(Base64.decode(this, Base64.DEFAULT))
+}
+
+fun String.toHex(): String {
+    return this.toByteArray().joinToString(separator = "") { "%02x".format(it) }
+}
+
+fun String.hexToString(): String {
+    return String(
+        this.chunked(2)
+            .map { it.uppercase().toInt(16).toByte() }
+            .toByteArray()
+    )
+}
+
+fun String.toUcs2(): ByteArray {
+    val charset = Charsets.UTF_16LE
+    return this.toByteArray(charset)
+}
+
+fun ByteArray.ucs2ToString(): String {
+    val charset = Charsets.UTF_16LE
+    return String(this, charset)
+}
+
+fun String.toUTF16LE(): ByteArray {
+    val charset = Charsets.UTF_16LE
+    return this.toByteArray(charset)
+}
+
+fun ByteArray.UTF16LEToString(): String {
+    val charset = Charsets.UTF_16LE
+    return String(this, charset)
+}
+
+fun String.toLatin1(): ByteArray {
+    val charset = Charset.forName("latin1")
+    return this.toByteArray(charset)
+}
+
+fun ByteArray.latin1ToString(): String {
+    val charset = Charset.forName("latin1")
+    return String(this, charset)
 }
